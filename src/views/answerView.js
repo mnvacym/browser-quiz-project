ce1ba2849a5bc116a1a2248f9e463338a7589b7e
Muhie Aldeen <zezo1998@hotmail.com>
2023-04-25 16:29:56 +0200

    I did not commit each step because I forgot to create a new brach so I just created it and coped the code I have wrettin from master on my local repo to the new brach anyway, Iwrote a function to view the question insde the function I declered a variable current question then I used the innerHTml to view the question after that I used for of loop to loop over the answers and add the chosen one to the answer list then I got tle next question button and added eventListner to onclick the second parameter is a function incrementing the key object currentQuestionIndex and call back the viewQuestion function

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -1,11 +1,31 @@
+import { quizData } from "../data";
+
+ 
 /**
  * Create an Answer element
  * @returns {Element}
  */
+
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
   return element;
+};
+
+
+const viewQuestion() ={
+let currentQuestion = quizData[quizData.currentQuestionIndex];
+const userInterface = document.getElementById('user-interface');
+userInterface.innerHTML = createQuestionElement(currentQuestion);
+
+for (const element of Object.entries(currentQuestion)) {
+ element.textContent = answerText ;
+  answerListElement.appendChild(element);
+}
+document.getElementById('next-question-button').addEventListener('click', ()=> {
+  currentQuestionIndex++;
+  viewQuestion();
+});
 };
