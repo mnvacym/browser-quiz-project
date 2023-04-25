b248e914d4457c6d823d1126a29985b90c074f5d
marwan <marwan.h1994@gmail.com>
2023-04-26 00:07:27 +0200

    function check the answer true or fasle and showed the true answer

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -45,7 +45,6 @@ const nextQuestion = () => {
   initQuestionPage();
 };
 
-
 function checkAnswer(questionIndex, selectedAnswer) {
   const currentQuestion = quizData.questions[questionIndex];
   const correctAnswer = currentQuestion.correct;
@@ -70,5 +69,7 @@ const result = checkAnswer(questionIndex, selectedAnswer);
 if (result.isCorrect) {
   console.log('Correct answer!');
 } else {
-  console.log(`Incorrect answer. The correct answer is ${result.correctAnswer}.`);
+  console.log(
+    `Incorrect answer. The correct answer is ${result.correctAnswer}.`
+  );
 }
