d9e14277c375d9dadbf93ab185e698fb4ac52bc5
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-04-26 11:56:02 +0200

    Fixed the bug in the Views/welcomeViews.js line 14 and line 15

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -11,11 +11,8 @@ export const createWelcomeElement = () => {
 
   element.innerHTML = String.raw`
 
-    <h1 class='welcome-heading'>Welcome to our quiz App</h1>
-    <button class='welcome-button' id="${START_QUIZ_BUTTON_ID}">start quiz</button>
-
-    <h1>Want to know more about the Netherlands? Have fun with our quiz🚀</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
+    <h1 class='welcome-heading'>Want to know more about the Netherlands? Have fun with our quiz🚀</h1>
+    <button class='welcome-button'  id="${START_QUIZ_BUTTON_ID}">start quiz</button>
 
   `;
 
