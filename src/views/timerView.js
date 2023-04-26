ffafbafb37917597ecd90a19d878fb5fbac45d12
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-04-26 17:39:14 +0200

    import timmer template literals (Template strings)

---
+++ src/views/timerView.js
@@ -0,0 +1,17 @@
+import { TIMER_QUESTION_ID } from '../constants.js';
+
+export const createTimer = (timer) => {
+  // div timer
+  const timerElement = Object.assign(document.createElement('div'), {
+    className: 'timmer-div',
+  });
+
+  console.log(timerElement);
+  // I use String.raw just to get fancy colors for the HTML in VS Code.
+  timerElement.innerHTML = String.raw`
+       <h1> ${timer}</h1>
+       <p id='${TIMER_QUESTION_ID}'></p>
+     `;
+
+  return timerElement;
+};
