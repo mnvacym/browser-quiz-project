7c3a9ccefdc9b3ec14756f95d78cdddde2b0d15f
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-04-28 13:44:31 +0200

    modify initQuestionPage

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,6 +1,7 @@
 import { ANSWERS_LIST_ID } from '../constants.js';
 import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
 
+
 /**
  * Create a full question element
  * @returns {Element}
@@ -9,10 +10,10 @@ export const createQuestionElement = (question) => {
   const element = Object.assign(document.createElement('div'), {
     className: 'question-div',
   });
+  // <div id="${TIMER_QUESTION_ID}"></div>
 
   // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
-  
     <h1>${question}</h1>
 
     <ul class='answer-ul' id="${ANSWERS_LIST_ID}">
