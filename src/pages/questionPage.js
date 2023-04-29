55d6e615c378da620b9ef50fef2788c10fc57f9f
marwan <marwan.h1994@gmail.com>
2023-04-29 14:35:35 +0200

    giveUp function is completed and i added it to answer list div to be more responsive

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -3,11 +3,14 @@ import {
   NEXT_QUESTION_BUTTON_ID,
   USER_INTERFACE_ID,
 } from '../constants.js';
-
+import { createGiveUpButton } from '../views/giveUpView.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
-import { showGiveUpButton } from './giveUpPage.js';
+//----------- select give up parent element--------->
+const giveUpParent = document.querySelector('#user-interface #give-up');
+const giveUpButton = createGiveUpButton();
+//----------- End select give up parent element----->
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
@@ -24,7 +27,19 @@ export const initQuestionPage = () => {
     const theCorrectAnswer = currentQuestion.correct;
     const answerElement = createAnswerElement(key, answerText);
     const allOptions = document.querySelector('.answer-ul').children;
+    // ---------------------------------   give up button --------------------------//
+    giveUpButton.addEventListener('click', () => {
+      for (const option of allOptions) {
+        if (option.innerText[0] === theCorrectAnswer) {
+          option.classList.add('correct');
+        }
+        option.classList.add('disabled');
+      }
+    });
+    userInterface.appendChild(giveUpButton);
+    // ---------------------------------   End give up button --------------------------//
 
+    //---------------------------------- select Answer && check Answer -----------------//
     answerElement.addEventListener('click', (event) => {
       const selectedElement = event.target;
       if (key === theCorrectAnswer) {
@@ -51,10 +66,10 @@ export const initQuestionPage = () => {
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
 };
+//---------------------------------- end select Answer && check Answer -----------------//
 
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
   initQuestionPage();
-  showGiveUpButton();
 };
