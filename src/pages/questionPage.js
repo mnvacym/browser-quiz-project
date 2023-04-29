3a0b609193c604391eb09a5a59447de37d4c3d51
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-04-29 22:27:21 +0200

    add initResetQuiz for reset button

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -3,25 +3,16 @@ import {
   NEXT_QUESTION_BUTTON_ID,
   USER_INTERFACE_ID,
   USER_HEADER_INTERFACE_ID,
+  TIMER_QUESTION_ID,
   START_QUIZ_BUTTON_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 import { createResetQuiz } from '../views/resetQuizView.js';
-
-export const initResetQuiz = () => {
-  const userHeaderInterface = document.getElementById(USER_HEADER_INTERFACE_ID);
-  userHeaderInterface.innerHTML = '';
-  const resetButton = createResetQuiz();
-  userHeaderInterface.appendChild(resetButton);
-  // resetButton.style.display = 'none';
-
-  // const startQuizButton = document.getElementById(START_QUIZ_BUTTON_ID);
-  // startQuizButton.addEventListener('click', function () {
-  //   resetButton.style.display = 'block';
-  // });
-};
+import { initWelcomePage } from './welcomePage.js';
+import { initResetTimer } from './timerPage.js';
+import { initTimer } from './timerPage.js';
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -71,4 +62,29 @@ const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
   initQuestionPage();
+};
+
+let quizStart = false;
+
+export const initResetQuiz = () => {
+  const userHeaderInterface = document.getElementById(USER_HEADER_INTERFACE_ID);
+  userHeaderInterface.innerHTML = '';
+  const resetButton = createResetQuiz();
+  userHeaderInterface.appendChild(resetButton);
+  userHeaderInterface.style.display = 'block';
+
+  const timer = document.getElementById(TIMER_QUESTION_ID);
+
+  timer.style.display = 'block';
+  resetButton.addEventListener('click', () => {
+    if (quizStart === true) {
+      timer.style.display = 'block';
+      initTimer();
+    } else {
+      timer.style.display = 'none';
+    }
+    initWelcomePage();
+    initResetTimer();
+    userHeaderInterface.style.display = 'none';
+  });
 };
